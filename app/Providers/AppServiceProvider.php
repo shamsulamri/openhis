229dcc66189eeb35aa74acaffb9885b305fcd51a
graymatter <shamsulamrimd@gmail.com>
2020-11-02 12:14:11 +0800

    Force https for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,9 @@ class AppServiceProvider extends ServiceProvider
 				Log::info($query->time);
 		});
 		 */
-		$this->app['request']->server->set('HTTPS','on');
+		if (env('APP_ENV') === 'production') {
+			$this->app['request']->server->set('HTTPS','on');
+		}
 
 		Validator::extend('greater_than_or_equal', function($attribute, $value, $parameters, $validator) {
 				if ($value==$parameters[1]) {
